97dc38a94e65168d549d3ddf72c4d19026623092
hmartinez00 <hectoralonzomartinez00@gmail.com>
2023-07-08 13:04:04 -0400

    Version Estable 20230708 130404

--- scripts/index_update.py
+++ scripts/index_update.py
@@ -1,17 +1,26 @@
 import os
+import time
 
 curso='python311'
 clase='Intro'
+source_file=os.path.join(curso, clase + '.html')     # ruta_del_archivo_a_monitorear
+destination_file=os.path.join('../reveal.js', 'index.html') # ruta_del_archivo_destino
 
-file_start=os.path.join(curso, clase + '.html')
-file_end=os.path.join('../reveal.js', 'index.html')
+def compare_and_update(source_file, destination_file):
+    from datetime import datetime as dt
+    with open(source_file, 'r', encoding='utf-8') as source:
+        source_content = source.read()
+        source.close()
+    with open(destination_file, 'r', encoding='utf-8') as destination:
+        destination_content = destination.read()
+        destination.close()
+    if source_content != destination_content:
+        f = open(destination_file, 'w', encoding='utf-8')
+        f.write(source_content)
+        f.close()
+        time_ud=dt.now().strftime('%H:%M:%S')
+        print(f'[{time_ud}] Actualizado.')
 
-f = open(file_start, 'r', encoding='utf-8')
-string=f.read()
-f.close()
-
-f = open(file_end, 'w', encoding='utf-8')
-f.write(string)
-f.close()
-
-print(file_start)
+while True:
+    compare_and_update(source_file, destination_file)
+    time.sleep(1)
