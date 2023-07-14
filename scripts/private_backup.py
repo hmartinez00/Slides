d821d53182a594214b76af46e69c9f085da3383d
hmartinez00 <hectoralonzomartinez00@gmail.com>
2023-07-14 13:15:46 -0400

    Version Estable 20230714 131545

--- scripts/private_backup.py
+++ scripts/private_backup.py
@@ -1,13 +1,33 @@
 import os
+import shutil
 
-dir = os.getcwd()
-project = 'laravel/ejemplo'
-file = '.gitignore'
+dir     = os.path.join(os.getcwd(), 'laravel')
+project = os.path.join(dir, 'ejemplo')
+file    = '.gitignore'
 
-path = os.path.join(dir, project, file)
+ignorefile_path = os.path.join(project, file)
 
-with open(path, encoding='utf-8') as ignorefile:
-    content = ignorefile.read()
-    print(content)
+with open(ignorefile_path, encoding='utf-8') as ignorefile:
+    content = [str(i).split('\n')[0] for i in ignorefile.readlines()]
+    ignorefile.close()
 
-ignorefile.close()
+print(content)
+
+def copiar_elemento(elemento, directorio_origen, directorio_destino):
+    import os
+    ruta_elemento   = os.path.join(directorio_origen, elemento)
+    ruta_destino    = os.path.join(directorio_destino, elemento)
+    if os.path.isdir(ruta_elemento):
+        shutil.copytree(ruta_elemento, ruta_destino)
+        print(f"Directorio '{elemento}' copiado a '{directorio_destino}' de forma recursiva.")
+    elif os.path.isfile(ruta_elemento):
+        shutil.copy2(ruta_elemento, ruta_destino)
+        print(f"Archivo '{elemento}' copiado a '{directorio_destino}'.")
+    else:
+        print(f"El elemento '{elemento}' no existe en el directorio de origen.")
+
+directorio_origen   = project
+directorio_destino  = os.path.join('C:', 'Users', 'Hector', 'Mi unidad (hmbackups00@gmail.com)', 'laravel_projects', 'ejemplo')  # Reemplaza con la ruta del directorio de destino
+
+for elemento in content:
+    copiar_elemento(elemento, directorio_origen, directorio_destino)
--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,3 @@
-import sys
-
-print(sys.executable)
+import os
+dir = os.path.join('C:', 'Users', 'Hector', 'Mi unidad (hmbackups00@gmail.com)')
+print(os.listdir(dir))
