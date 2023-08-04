a97497b98b19cfe3b8a900680a322994dafc4f42
hmartinez <hmartinez@abae.gob.ve>
2023-08-04 18:55:10 -0400

    Version Estable 20230804 185503

--- scripts/sys_clean.py
+++ scripts/sys_clean.py
@@ -1,3 +1,13 @@
+import tkinter as tk
 from General_Utilities.sys_clean import sys_clean
 
-sys_clean()
+def ejecutar_programa():
+    sys_clean()
+
+def crear_interfaz():
+    ventana = tk.Tk()
+    boton = tk.Button(ventana, text="Ejecutar programa", command=ejecutar_programa)
+    boton.pack()
+    ventana.mainloop()
+
+crear_interfaz()
--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,15 @@
-from General_Utilities.sys_clean import sys_clean
-
-sys_clean()
+import tkinter as tk
+import subprocess
+def ejecutar_programa():
+    # Ruta al programa que deseas ejecutar
+    ruta_programa = "ruta_al_programa.exe"  # Reemplaza con la ruta correcta
+     # Ejecuta el programa utilizando subprocess
+    subprocess.call(ruta_programa)
+ # Crear una ventana
+ventana = tk.Tk()
+ # Definir elementos de la interfaz
+boton = tk.Button(ventana, text="Ejecutar programa", command=ejecutar_programa)
+ # Colocar los elementos en la ventana
+boton.pack()
+ # Iniciar el bucle de eventos
+ventana.mainloop()
