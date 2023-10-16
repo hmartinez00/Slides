637fbe281e9a54de1b50bf1b6fe8e92846565fb8
hmartinez <hmartinez@abae.gob.ve>
2023-10-16 11:57:40 -0400

    Version Estable 20231016 115739

--- scripts/backend_menu.py
+++ scripts/backend_menu.py
@@ -1,5 +1,5 @@
 from General_Utilities.menu import menu_class
-from module.backend_tools import laravel_orders
+from General_Utilities.backend_tools import laravel_orders
 
 cls = laravel_orders()
 
